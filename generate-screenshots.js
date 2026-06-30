2b8df79da405218eb297d8938d5e97dbab2989a6
Elliot <elliot.a.richards@gmail.com>
2026-06-30 07:43:22 +0100

    Removing GitHub Pages references

--- generate-screenshots.js
+++ generate-screenshots.js
@@ -10,7 +10,8 @@ async function generateScreenshots() {
   const context = await browser.newContext();
   
   // Base URL - GitHub Pages live site
-  const baseURL = 'https://jdrhyne.github.io/volks-typo';
+  // const baseURL = 'https://jdrhyne.github.io/volks-typo';
+  const baseURL = 'https://elliotx.com';
   
   console.log('🎨 Generating Volks-Typo theme screenshots for submission...\n');
   
